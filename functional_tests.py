8f6d6fda887de05d0ad2ccd984fda024bb1f72c8
Punit Jain <contactpunitjain@gmail.com>
2020-05-17 13:11:49 +0530

    Added unittests to structure functional tests.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,29 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class TestCostEstimator(unittest.TestCase):
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_title_of_home_page(self):
+        "Test title is shown as Cost Estimator for the project"
+        # First user enters url and sees a page with title "Cost Estimator"
+        self.browser.get('http://localhost:8000')
+
+        # Page title "Cost Estimator"
+        self.assertIn("Cost Estimator", self.browser.title)
+        self.fail("Finish test")
+
+        # User sees From input box to enter travel from
+
+        # User sees Destination input box to enter travel to
+
+        # User ses submit button to click on search for itineraries
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
