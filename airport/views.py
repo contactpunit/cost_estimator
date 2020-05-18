a39149bba169a6635f4e817e78692d0d2d4ce251
Punit Jain <contactpunitjain@gmail.com>
2020-05-18 21:52:35 +0530

    Added code for get_airport_by_name

--- airport/views.py
+++ airport/views.py
@@ -7,10 +7,12 @@ import os
 import sys
 
 
-# Create your views here.
+#
+# # Create your views here.
 class AirportTracker:
-    def __init__(self, place):
-        self.place = place
+    def __init__(self, **kwargs):
+        self.place = kwargs.get('place', None)
+        self.country_code = kwargs.get('country_code', None)
         self.place_type = None
         self.url = None
         self.params = None
@@ -28,12 +30,28 @@ class AirportTracker:
                         self.rapidapi_key_name: self.rapidapi_key_value, 'Content-Type': 'application/json'}
 
     def get_airport_by_name(self):
-        pass
+        self.place_type = 'name'
+        self._set_url_params()
+        response = self.http_request()
+        if response:
+            for record in response:
+                if record['name'] == self.place and record['countryCode'] == self.country_code:
+                    return record
+            raise ValueError('Invalid place. Please enter correct value')
+        if not response:
+            self.get_airports_by_radius(radius=200, longitude=)
+        return response
 
     def get_airport_by_code(self):
+        self.place_type = 'code'
         self._set_url_params()
+        response = self.http_request()
+        return response
 
     def get_nearest_airports(self):
+        pass
+
+    def get_airports_by_radius(self):
         pass
 
     def _set_url_params(self):
@@ -44,27 +62,24 @@ class AirportTracker:
             self.url = self.airport_by_name_url
             self.params = {"text": self.place}
 
-    def execute(self):
+    def http_request(self):
         try:
-            print(self.headers)
-            print(self.params)
-            print(self.url)
             response = requests.request("GET", self.url, headers=self.headers, params=self.params)
-            print(response.text)
+            if response.status_code == 200:
+                return response.json()
         except requests.exceptions.Timeout as e:
             print(f'Request timed out. Reason - {e}')
-            raise
         except requests.exceptions.RequestException as e:
             print(f'Error while making request. Error - {e}')
 
     def __call__(self, *args, **kwargs):
         if len(self.place) == 3:
-            self.place_type = 'code'
-        if len(self.place) > 3:
-            self.place_type = 'name'
-        self._set_url_params()
-        self.execute()
+            self.get_airport_by_code()
+        elif len(self.place) > 3:
+            self.get_airport_by_name()
+        else:
+            raise ValueError('Invalid place name. Please enter either code or full name')
 
 
-a = AirportTracker('PNQ')
-a()
+a = AirportTracker(place='latur', country_code='IN')
+print(a())
