3c2222050a1cf6c4a421406ca8e87b52a89405ee
Punit Jain <contactpunitjain@gmail.com>
2020-05-19 12:21:03 +0530

    added tests to flight view

---
+++ manageflights/test_flights.py
@@ -0,0 +1,13 @@
+import pytest
+from manageflights.views import FlightTracker
+
+def test_get_flight_from_source_to_destination():
+    f = FlightTracker('source', 'destination')
+
+
+def test_get_price_of_flight():
+    pass
+
+
+def get_flights_from_nearest_airport():
+    pass
--- manageflights/views.py
+++ manageflights/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+class FlightTracker:
+    def __init__(self, source, destination):
+        self.source = source
+        self.destination = destination
