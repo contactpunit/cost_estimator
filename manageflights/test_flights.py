10b591236c0955d0d8803cf0ace36d87a8fd2fac
Punit Jain <contactpunitjain@gmail.com>
2020-05-19 16:08:32 +0530

    added test cases and view for flight

--- manageflights/test_flights.py
+++ manageflights/test_flights.py
@@ -1,8 +1,14 @@
 import pytest
 from manageflights.views import FlightTracker
 
+
+def test_repr_of_class():
+    f = FlightTracker('pnq', 'IN', 'del', 'IN', 2)
+    assert repr(f) == '(FlightTracker) from pnq to del for 2 travellers'
+
+
 def test_get_flight_from_source_to_destination():
-    f = FlightTracker('source', 'destination')
+    pass
 
 
 def test_get_price_of_flight():
--- manageflights/views.py
+++ manageflights/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 class FlightTracker:
-    def __init__(self, source, destination):
+    def __init__(self, source, s_country, destination, d_country, num_travellers):
         self.source = source
         self.destination = destination
+        self.source_country = s_country
+        self.dest_country = d_country
+        self.num_travellers = num_travellers
+
+    def __repr__(self):
+        return f'({self.__class__.__name__}) from {self.source} to {self.destination} for {self.num_travellers} travellers'
