580cbf4c1856aeb7c2d52a113c396a018ea81375
Punit Jain <contactpunitjain@gmail.com>
2020-05-23 11:58:26 +0530

    changed urls and templates

--- travel/models.py
+++ travel/models.py
@@ -1,14 +1,3 @@
 from django.db import models
 
-
 # Create your models here.
-class Travel(models.Model):
-    source = models.TextField()
-    destination = models.TextField()
-    country = models.TextField()
-    travel_date = models.DateField(auto_now=True)
-    num_travellers = models.IntegerField()
-
-    def __str__(self):
-        return f'Travel details - From {self.source} To {self.destination} DateOfTravel {self.travel_date} ' \
-               f'NumPassengers {self.num_travellers} '
