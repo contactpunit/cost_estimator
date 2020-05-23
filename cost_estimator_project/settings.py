750b3f294c3e8144d6ac83c56372202d3d669460
Punit Jain <contactpunitjain@gmail.com>
2020-05-23 09:52:45 +0530

    added base template directory and test view

--- cost_estimator_project/settings.py
+++ cost_estimator_project/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'cost_estimator_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'cost_estimator_project/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- travel/views.py
+++ travel/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def get_itinerary(request):
     pass
+
+
+def index(request):
+    return render(request, 'home.html')
