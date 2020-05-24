d41c284d06988f4510f0bd9f575b97833513d9a9
Punit Jain <contactpunitjain@gmail.com>
2020-05-24 16:29:20 +0530

    added weather app as a util

---
+++ utilities/weather_utils.py
@@ -0,0 +1,19 @@
+from utilities.utils import ConfigReaderMixin, http_request
+
+
+class WeatherTracker(ConfigReaderMixin):
+    def __init__(self, country=None, destination=None):
+        self.country = country
+        self.destination = destination
+        self.params = {'units': self.units, 'q': ','.join([self.destination, self.country]),
+                       self.weather_api_key: self.weather_api_value}
+        self.headers = {'Content-Type': 'application/json'}
+
+    def get_weather_details(self):
+        url = self.weather_url
+        response = http_request(url=url, params=self.params, headers=self.headers)
+        return response
+
+
+w = WeatherTracker(country='India', destination='Pune')
+w.get_weather_details()
