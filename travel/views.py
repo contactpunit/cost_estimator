6868ca2aab75f7613c7212ff8cb843264352bba1
Bob Belderbos <bobbelderbos@gmail.com>
2020-05-26 19:59:11 +0200

    as per code clinic, using one view, also made shorter lines (flake8) next is to populate results in template

--- travel/views.py
+++ travel/views.py
@@ -1,25 +1,26 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from .forms import TravelForm
 from utilities.travel_utils import Travel
 
 
-# Create your views here.
-
 def itinerary(request):
     form = TravelForm(request.POST or None)
+    data = {}
     if form.is_valid():
         source = form.cleaned_data['source']
         destination = form.cleaned_data['destination']
         country = form.cleaned_data['country']
         travel_date = form.cleaned_data['travel_date']
         num_passengers = form.cleaned_data['num_passengers']
-        data = get_itinerary(source=source, destination=destination, country=country, travel_date=travel_date,
+        data = get_itinerary(source=source, destination=destination,
+                             country=country, travel_date=travel_date,
                              num_passengers=num_passengers)
-        return render(request, 'results.html', {'data': data})
-    return render(request, 'home.html', {'form': form})
+    return render(request, 'home.html', {'form': form, 'data': data})
 
 
-def get_itinerary(source=None, destination=None, country=None, travel_date=None, num_passengers=0):
-    t = Travel(source=source, destination=destination, country=country, travel_date=travel_date,
+def get_itinerary(source=None, destination=None, country=None,
+                  travel_date=None, num_passengers=0):
+    t = Travel(source=source, destination=destination,
+               country=country, travel_date=travel_date,
                num_passengers=num_passengers)
     return t.find_itineraries()
